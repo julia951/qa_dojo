f688ee814d22f57ee7b774bb9089cbf051683f9a
Yulka <yuilka@example.com>
2025-01-07 19:26:34 +0200

    6 DZ BONUS filled

---
+++ tests/v6/variables.ts
@@ -0,0 +1,18 @@
+// Створити змінні які б описували всі предмети на вашому робочому столі, і на столі на кухні (не менше 20 змінних) 
+
+
+const workLaptop = "Lenovo";
+const mouseWork = "RZTK";
+let pillsCount = 5;
+let certificationsCount = 2;
+let notesCount = 4;
+let figuresCount = 6;
+let joystick = 'Xbox';
+let microphohe = 'Razer';
+const mousePeronal = "MX Master 3";
+let pencilsCount = 7;
+const keyboard = 'Asus TUF Gaming';
+const monitor = 'Dell U2720Q';
+const deskLamp = 'IKEA RANARP';
+const headphones = 'JBL Tune 520BT Black ';
+let monitorsCount = 2;
