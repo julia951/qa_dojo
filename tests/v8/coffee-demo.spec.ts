01f9b3a9c1bc2ec4e039226135173dad96194430
Yulka <yuilka@example.com>
2025-01-22 11:37:55 +0200

    add new and remove uncessecary cases

--- tests/v8/coffee-demo.spec.ts
+++ tests/v8/coffee-demo.spec.ts
@@ -44,16 +44,43 @@ async function espressoClick(page: Page) {
   await espresso.click();
 }
 
-async function flatWhiteClick(page: Page) {
-  const flatWhite = page.locator('[data-test="Flat_White"]');
-  await flatWhite.click();
+async function espressoRemove(page: Page) {
+  const deleteEspresso = page.locator('[aria-label="Remove one Espresso"]');
+  await deleteEspresso.click();
 }
 
-test('CC-001 Test successful order Multiple Coffees', async () => {
+async function hoverCheckout(page: Page) {
+  const totalBtn = page.locator('[data-test="checkout"]');
+  await totalBtn.hover();
+}
+
+async function openCartTab(page: Page) {
+  const cartTab = page.locator('[aria-label="Cart page"]');
+  await cartTab.click();
+}
+
+async function clickRemoveIcon(page: Page) {
+  const deleteIcon = page.locator('[aria-label="Remove all Espresso"]');
+  await deleteIcon.click();
+}
+
+test('CC-001 Test successful order espresso Coffees', async () => {
  
   const successText = "Thanks for your purchase. Please check your email for payment.";
 
-  const espresso = page.locator('[data-test="Espresso"]');
+  const successMsg = page.locator('[class="snackbar success"]');
+
+  espressoClick(page);
+  openCart(page);
+  purchase(page, usernameText, emailText);
+
+  await expect(successMsg).toContainText(successText);
+});
+
+test('CC-002 Test successful order Multiple Coffees', async () => {
+ 
+  const successText = "Thanks for your purchase. Please check your email for payment.";
+
   const successMsg = page.locator('[class="snackbar success"]');
 
   espressoClick(page);
@@ -65,53 +92,62 @@ test('CC-001 Test successful order Multiple Coffees', async () => {
   await expect(successMsg).toContainText(successText);
 });
 
+test('CC-003 Test Changing items By Plus Button', async () => {
 
-test('CC-002 Test successful Order By Plus Button', async () => {
-
-  const successText = "Thanks for your purchase. Please check your email for payment.";
-
-  const totalBtn = page.locator('[data-test="checkout"]');
-  const addEspresso = page.locator('[aria-label="Add one Espresso"]');
-  const successMsg = page.locator('[class="snackbar success"]');
+  const totalPtice = 'Total: $20.00';
+  const checkout = page.locator('[data-test="checkout"]');
+  const quantityText = page.locator('[class="unit-desc"]');
 
   espressoClick(page);
-  await totalBtn.hover();
-  await addEspresso.click();
-
-  openCart(page);
-
-  purchase(page, usernameText, emailText);
-
-  await expect(successMsg).toContainText(successText); 
+  espressoClick(page);
+  hoverCheckout(page);
+  
+  await expect(checkout).toContainText(totalPtice);
+  await expect(quantityText).toContainText('x 2');
 });
 
-test('CC-003 Test Visibility Of Cafe Latte Item', async () => {
+test('CC-004 Test Deleting Item from Cart Usign -', async () => {
 
-  const cafeLatte = page.locator('[data-test="Cafe_Latte"]');
-
-  await expect(cafeLatte).toBeVisible();
-});
-
-test('CC-004 Test Flat White Total Price Calculation', async () => {
-
-  const flatWhiteTotal = 'Total: $18.00';
-
+  const zeroTotalPrice = 'Total: $0.00';
   const checkout = page.locator('[data-test="checkout"]');
 
-  flatWhiteClick(page);
+  await espressoClick(page);
+  await hoverCheckout(page);
+  await espressoRemove(page);
 
-  await expect(checkout).toContainText(flatWhiteTotal);
+  await expect(checkout).toContainText(zeroTotalPrice);
 });
 
+test('CC-005 Test Deleting Item from Cart Tab', async () => {
+  const deleteIcon = page.locator('[class="list"]');
 
-test('CC-005 Test CheckBox Message Visibility', async () => {
+  await espressoClick(page);
+  await openCartTab(page);
+  await clickRemoveIcon(page);
 
-  const promotionMessage = page.locator("[aria-label='Promotion message']");
-  const promoMsg = 'I would like to receive order updates and promotional messages.';
+  await expect(deleteIcon).toContainText('No coffee, go add some.');
 
-  espressoClick(page);
+});
 
+test('CC-006 Test Lucky Banner', async () => {
+  const luckyBanner = 'It\'s your lucky day! Get an extra cup of Mocha for $4.';
+
+  await espressoClick(page);
+  await espressoClick(page);
+  await espressoClick(page);
+
+  await expect(page.locator('#app')).toContainText(luckyBanner);
+});
+
+test('CC-007 Test Closing Cart with Full Items', async () => {
+
+  const closeBtn = page.locator('[class="close"]');
+  const totalPtice = 'Total: $10.00';
+  const checkout = page.locator('[data-test="checkout"]');
+
+  await espressoClick(page);
   await openCart(page);
+  await closeBtn.click();
 
-  await expect(promotionMessage).toContainText(promoMsg);
+  await expect(checkout).toContainText(totalPtice);
 });
