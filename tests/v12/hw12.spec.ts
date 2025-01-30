1ada51997d62bc9b8cae0d97e40992d8abb617ab
Yulka <yuilka@example.com>
2025-01-30 10:29:11 +0200

    add function - constructor

--- tests/v12/hw12.spec.ts
+++ tests/v12/hw12.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 
 /* 1. Написати параметризований тест для форми https://demoqa.com/automation-practice-form
 2. Дата провайдер повинен мати мінімум 5 разних наборів данних. 
@@ -6,12 +6,22 @@ import { test, expect } from "@playwright/test";
 
 (приклад дивіться на відео https://t.me/c/2456196894/83/749) */
 
+function RegisterPage(page: Page) {
+    return {
+        page,
 
-function hiMario() {
-    console.log("It`s me Mario");
-}
-function hiLuigi() {
-    console.log("It`s me Luigi");
+        async navigateTo() {
+            await this.page.goto('https://demoqa.com/automation-practice-form');
+        },
+
+        async fillRegisterForm(registerData: { firstName: string, lastName: string, telephoneNumber: string }) {
+            await this.page.getByPlaceholder("First Name").fill(registerData.firstName);
+            await this.page.getByPlaceholder("Last Name").fill(registerData.lastName);
+            await this.page.getByText('Male', { exact: true }).click();
+            await this.page.getByPlaceholder('Mobile Number').fill(registerData.telephoneNumber);
+            await this.page.getByRole('button', { name: 'Submit' }).click();
+        }
+    };
 }
 
 const authData = [
@@ -21,7 +31,6 @@ const authData = [
         lastName: "aqsfasf",
         telephoneNumber: "0000000000",
         isSuccessful: true,
-        func: hiMario,
     },
     {
         testId: "cond-2",
@@ -29,7 +38,6 @@ const authData = [
         lastName: "aqsfasf",
         telephoneNumber: "000000000",
         isSuccessful: false,
-        func: hiMario,
     },
     {
         testId: "cond-3",
@@ -37,7 +45,6 @@ const authData = [
         lastName: "aqsfasf",
         telephoneNumber: "",
         isSuccessful: false,
-        func: hiMario,
     },
     {
         testId: "cond-4",
@@ -45,7 +52,6 @@ const authData = [
         lastName: "aqsfasf",
         telephoneNumber: "someUsername",
         isSuccessful: false,
-        func: hiMario,
     }
 ];
 
@@ -55,22 +61,13 @@ for (const {
     lastName,
     telephoneNumber,
     isSuccessful,
-    func,
 } of authData) {
-    test(`${testId} Try to register without mandatory field`, async ({
-        page,
-    }) => {
-        await page.goto("https://demoqa.com/automation-practice-form");
+    test(`${testId} Try to register without mandatory field`, async ({ page }) => {
+        
+        const registerPage =  RegisterPage(page);
+        await registerPage.navigateTo();
+        await registerPage.fillRegisterForm({ firstName, lastName, telephoneNumber });
 
-        func();
-        await page.getByPlaceholder("First Name").fill(firstName);
-        await page.getByPlaceholder("Last Name").fill(lastName);
-        await page.getByText('Male', { exact: true }).click();
-        await page.getByPlaceholder('Mobile Number').fill(telephoneNumber);
-        //await page.getByPlaceholder("Password").fill(Password);
-        await page.getByRole('button', { name: 'Submit' }).click();
-
-        func();
 
         if (isSuccessful) {
             await expect(page.locator('#example-modal-sizes-title-lg')).toContainText('Thanks for submitting the form');
